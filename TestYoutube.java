1797a6621c8634fc9115a87f8c25ba4970694694
NaoY220 <129801469+NaoY220@users.noreply.github.com>
2024-06-04 14:11:20 -0700

    Update TestYoutube.java

--- TestYoutube.java
+++ TestYoutube.java
@@ -1,1 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.*;
+import org.junit.*;
 
+public class TestYoutube{
+
+  @Test
+  public void test() {
+    List<String> l1 = new ArrayList<String>(Arrays.asList("x", "y"));
+		List<String> l2 = new ArrayList<String>(Arrays.asList("a", "b"));
+		assertArrayEquals(new String[]{ "a", "b", "x", "y"}, ListExamples.merge(l1, l2).toArray());
+  }
